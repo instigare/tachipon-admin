a13a0ac9a7936649492e9c331e6bc10f84e03eff
alvi kazi <alvi@kazi.sh>
2020-01-07 21:59:56 +0900

    Added basic image upload support

--- tachipon/urls.py
+++ tachipon/urls.py
@@ -16,8 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('manager/', include('django.contrib.auth.urls')),
     path('manager/', include('manager.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}),
+    ]
